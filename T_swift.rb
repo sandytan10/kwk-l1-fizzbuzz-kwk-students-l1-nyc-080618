266124b71344890767aba07daa7b664065da8d7d
sandytan10 <stan10@stuy.edu>
2018-08-08 14:26:37 +0000

    automatically backed up by learn

---
+++ T_swift.rb
@@ -0,0 +1,16 @@
+def shake_it_off
+  play_count = 0  
+  puts "Cause the players gonna"
+  while play_count < 5
+  puts "play "
+  play_count = play_count + 1 
+  end
+  hate_count = 0 
+  puts "And the haters gonna hate"
+  while hate_count < 5
+  puts "hate"
+  hate_count = hate_count + 1
+  end 
+end 
+
+shake_it_off
---
+++ dancing.rb
@@ -0,0 +1,13 @@
+#write a wild loop
+#output "step to the left"/"step to right" 5 time
+
+counter = 0 
+while counter < 5
+puts "step to the left"
+puts "step to the right"
+counter = counter + 1 
+end 
+
+
+
+
